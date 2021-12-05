e2ba7fbd3bc30c5370ac2be1491f67c6d2387d97
yoshio.takatsuka <yoshio.takatsuka@gamil.com>
2021-12-05 15:18:28 +0900

    ナブバー追加

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-resevation-app'`, () => {
+  it(`should have as title 'resavation-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-resevation-app');
+    expect(app.title).toEqual('resavation-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-resevation-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('resavation-app app is running!');
   });
 });
